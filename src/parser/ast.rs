89801ec38241950952403df4305772462dce508e
JackMagee21 <mt.j07@yahoo.com>
2026-02-19 00:07:08 +0000

    Added FunctionCall and FunctionDef command types

--- src/parser/ast.rs
+++ src/parser/ast.rs
@@ -2,39 +2,38 @@
 
 #[derive(Debug, Clone)]
 pub enum Command {
-    /// ls -la /home
     Simple {
         args: Vec<String>,
         redirects: Vec<Redirect>,
         background: bool,
     },
-    /// cmd1 | cmd2 | cmd3
     Pipeline(Vec<Command>),
-    /// cmd1 && cmd2
     And(Box<Command>, Box<Command>),
-    /// cmd1 || cmd2
     Or(Box<Command>, Box<Command>),
-    /// cmd1 ; cmd2
     Sequence(Box<Command>, Box<Command>),
-
-    /// if <condition> { <body> } [else { <else_body> }]
     If {
         condition: Box<Command>,
         body: Vec<Command>,
         else_body: Option<Vec<Command>>,
     },
-
-    /// for <var> in <items> { <body> }
     For {
         var: String,
         items: Vec<String>,
         body: Vec<Command>,
     },
-
-    /// while <condition> { <body> }
     While {
         condition: Box<Command>,
         body: Vec<Command>,
+    },
+    /// User-defined function call
+    FunctionCall {
+        name: String,
+        args: Vec<String>,
+    },
+    /// Function definition
+    FunctionDef {
+        name: String,
+        body: Vec<String>,
     },
 }
 
